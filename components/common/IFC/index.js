dbb272167fad1c7061c1dbb6a17c349c0f5611e2
Konstantinos Kyrtsonis <kyrkost@outlook.com>
2022-07-06 22:06:10 +0530

    Adds picking and highlighting features

--- components/common/IFC/index.js
+++ components/common/IFC/index.js
@@ -8,7 +8,15 @@ import {
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
+  MeshLambertMaterial,
+  Raycaster,
+  Vector2
 } from "three";
+import {
+  acceleratedRaycast,
+  computeBoundsTree,
+  disposeBoundsTree
+} from 'three-mesh-bvh';
 import {
   OrbitControls
 } from "three/examples/jsm/controls/OrbitControls";
@@ -89,10 +97,97 @@ const IFC = () => {
       });
 
       // Sets up the IFC loading
+      const ifcModels = [];
       const ifcLoader = new IFCLoader.IFCLoader();
-      ifcLoader.ifcManager.setWasmPath("../../../../");
+      await ifcLoader.ifcManager.setWasmPath("../../../../");
+      ifcLoader.load("/ifcs/one.ifc", (ifcModel) => {
+        scene.add(ifcModel)
+        ifcModels.push(ifcModel);
+      });
 
-      ifcLoader.load("/ifcs/one.ifc", (ifcModel) => scene.add(ifcModel));
+      // === Picking ====
+      const raycaster = new Raycaster();
+      raycaster.firstHitOnly = true;
+      const mouse = new Vector2();
+
+      const cast = (event) => {
+        // Computes the position of the mouse on the screen
+        const bounds = threeCanvas.getBoundingClientRect();
+
+        const x1 = event.clientX - bounds.left;
+        const x2 = bounds.right - bounds.left;
+        mouse.x = (x1 / x2) * 2 - 1;
+
+        const y1 = event.clientY - bounds.top;
+        const y2 = bounds.bottom - bounds.top;
+        mouse.y = -(y1 / y2) * 2 + 1;
+
+        // Places it on the camera pointing to the mouse
+        raycaster.setFromCamera(mouse, camera);
+
+        // Casts a ray
+        return raycaster.intersectObjects(ifcModels);
+      }
+
+      const pick = (event) => {
+        const found = cast(event)[0];
+        if (found) {
+          const index = found.faceIndex;
+          const geometry = found.object.geometry;
+          const ifc = ifcLoader.ifcManager;
+          const id = ifc.getExpressId(geometry, index);
+        }
+      }
+
+      threeCanvas.ondblclick = pick;
+
+      // ==== Highlight ==== 
+      const preselectMat = new MeshLambertMaterial({
+        transparent: true,
+        opacity: 0.6,
+        color: 0xff88ff,
+        depthTest: false
+      })
+      const ifc = ifcLoader.ifcManager;
+      // Sets up optimized picking
+      ifc.setupThreeMeshBVH(computeBoundsTree, disposeBoundsTree, acceleratedRaycast);
+
+      // Reference to the previous selection
+      let preselectModel = { id: - 1 };
+
+      const highlight = (event, material, model) => {
+        const found = cast(event)[0];
+
+        if (found) {
+          // Gets model ID
+          model.id = found.object.modelID;
+
+          // Gets Express ID
+          const index = found.faceIndex;
+          const geometry = found.object.geometry;
+          const id = ifc.getExpressId(geometry, index);
+
+          const subsetConfig = {
+            modelID: 0,
+            ids: [id],
+            material: material,
+            scene: scene,
+            removePrevious: true,
+            applyBVH: true
+          }
+
+          // Creates subset
+          ifc.createSubset(subsetConfig)
+        } else {
+          // Removes previous highlight
+          ifc.removeSubset(model.id, material);
+        }
+      }
+
+      window.onmousemove = (event) => highlight(
+        event,
+        preselectMat,
+        preselectModel);
     }
     init()
   }, [])
