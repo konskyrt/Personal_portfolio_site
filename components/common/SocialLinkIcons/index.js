359c848014d2f193121bd43a23d0d3dfacbb7f10
Konstantinos Kyrtsonis <46296774+konskyrt@users.noreply.github.com>
2023-07-01 17:09:09 +0300

    Update LinkedIn site index.js

--- components/common/SocialLinkIcons/index.js
+++ components/common/SocialLinkIcons/index.js
@@ -7,7 +7,7 @@ const SocialLinkIcons = ({ isDarkIcons = false }) => {
     <Fade bottom delay={isDarkIcons ? 2500 : 0}>
       <div className='social-link-icons-component'>
         <div className={`social-acounts-container-${isDarkIcons ? 'dark' : 'light'}`}>
-          <a href='https://www.linkedin.com/in/konstantinoskirtsonis' target='_blank' rel='noreferrer'>
+          <a href='https://www.linkedin.com/in/konstantinoskyrtsonis' target='_blank' rel='noreferrer'>
             <LinkedinFilled style={{ fontSize: 30 }} />
           </a>
           <a href='https://github.com/konskyrt' target='_blank' rel='noreferrer'>
